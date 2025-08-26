39f58f17307b6a441bc07c3bfebb4a57c77e948e
shweta-1991 <shweta.dang91@gmail.com>
2025-08-26 22:00:00 +0530

    Update write.py

--- write.py
+++ write.py
@@ -1,6 +1,6 @@
 with open("text.txt","r") as reader:
-    content=reader.readlines()
-    list1=reversed(content)
+    content1=reader.readlines()
+    list1=reversed(content1)
     with open("text.txt","w") as writer:
         for line in list1:
 
